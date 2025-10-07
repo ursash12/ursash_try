ab8e2e611dee0d2a8a9900634334b941a5cb3bf4
Aditya raj <singhbikash1973@gmail.com>
2025-10-07 18:47:04 +0530

    new project calculator using c

--- c-lang.c
+++ c-lang.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 int main()
 {
-    printf("C lang");
+    int a,b;
+    int operator;
+    printf ("1.Addition 2.substraction 3.Multiplication 4.Division 5.Modulus Division");
+    scanf("%d",&operator);
+    printf("Enter two numbers");
+    scanf("%d%d",&a,&b);
+    switch(operator)
+    {
+        case 1 : printf("%d+%d=%d", a, b, a+b);
+         break;
+        case 2 : printf("%d-%d=%d", a, b, a-b);
+        break;
+        case 3 : printf("%d*%d=%d", a, b, a*b);
+        break;
+        case 4 : printf("%d/%d=%d", a, b, a/b);
+        break;
+        case 5 : printf("%d%%%d=%d", a, b, a%b);
+        break; 
+        default : printf("operator error");
+    }
     return 0;
 }
